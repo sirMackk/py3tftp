a506a66f4df340440dea9b8a5ebe9847fd42146e
sirMackk <c.tairix@gmail.com>
2016-02-15 17:19:11 +0100

    Adds module-wide logging

--- py3tftp.py
+++ py3tftp.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import asyncio
 from struct import Struct
 import os.path as opath
@@ -12,6 +13,10 @@ ERR = b'\x05\x00'
 READSIZE = 512
 TWOBYTE = Struct('=H')
 
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    level=logging.DEBUG)
+
 
 def byte_packer(length):
     return Struct('={length}H'.format(length=length))
@@ -22,25 +27,24 @@ class WRQServer(object):
 
 
 class RRQServer(object):
-    # add logging
     # add timeouts
     def __init__(self, rrq, addr):
-        print("creating rrq on {}".format(addr))
+        logging.info("Starting RRQServer, sending file to {}".format(
+            addr))
         self.addr = addr
         filename, _ = self.validate_req(*self.parse_req(rrq))
         self.file_iterator = self.get_file_iterator(filename)
         self.counter = 1
-        # rename
-        self.two_b_packer = byte_packer(1)
+        self.short_int = byte_packer(1)
 
     def validate_req(self, fname, mode):
         # validate format and opts
         return (fname.decode(encoding='ascii'), mode,)
 
     def parse_req(self, req):
-        print(req)
+        logging.debug("Reqest: {}".format(req))
         rq = req.split(b'\x00')
-        print(rq)
+        logging.debug("Parsed request: {}".format(rq))
         return rq[0], rq[1]
 
     def connection_made(self, transport):
@@ -48,33 +52,32 @@ class RRQServer(object):
         self.transport.sendto(next(self.file_iterator))
 
     def datagram_received(self, data, addr):
-        print("receiving dgram {}".format(len(data)))
-        print(self.correct_ack(data))
-        print(self.is_ack(data))
+        logging.debug("Receiving dgram, length: {}".format(len(data)))
         if self.is_ack(data) and self.correct_ack(data):
             try:
-                print("correct ack")
                 self.counter += 1
-                print("sending!")
+                logging.debug("sending!")
                 self.transport.sendto(next(self.file_iterator))
             except StopIteration:
-                print("file transfer finished")
+                self.info("File transfer complete")
                 self.transport.close()
+        else:
+            logging.debug("is_ack? {}".format(self.is_ack(data)))
+            logging.debug("correct_ack? {}".format(self.correct_ack(data)))
 
     def error_received(self, exc):
-        print("Error")
-        print(exc)
+        logging.error("Error receiving packet: {}".format(exc))
 
     def is_ack(self, data):
         return data[:2] == ACK
 
     def correct_ack(self, data):
-        ack_count, = self.two_b_packer.unpack(data[2:4])
+        ack_count, = self.short_int.unpack(data[2:4])
         return self.counter == ack_count
 
     def next_data_pkt(self):
         pkt = b''.join(DAT,
-                       self.two_b_packer.pack(self.counter),
+                       self.short_int.pack(self.counter),
                        next(self.file_iterator()))
         return pkt
 
@@ -100,16 +103,19 @@ class RRQServer(object):
                             break
                         yield contents
             except FileExistsError:
-                pass
+                logging.error("{} already exists! Cannot overwrite".format(
+                    fpath))
             except PermissionError:
-                pass
+                logging.error("Insufficient permissions to read {}".format(
+                    fpath))
             except FileNotFoundError:
-                pass
+                logging.error("{} does not exist!".format(fpath))
         return iterator()
 
     def connection_lost(self, exc):
-        print("conn lost")
-        print(exc)
+        logging.info("Connection to {0}:{1} terminated".format(*self.addr))
+        # might remove
+        logging.info(exc)
 
 
 class TftpServer(object):
@@ -117,54 +123,50 @@ class TftpServer(object):
         self.loop = loop
 
     def connection_made(self, transport):
-        print("conn made")
+        logging.info("Listening...")
         self.transport = transport
 
     def datagram_received(self, data, addr):
-        print("received: {}".format(data.decode()))
-        # match rrq or wrq or error
-        # listen = loop.create_datagram_endpoint(
-        # TftpServer,
-        # remote_addr=addr)
-        # run_until_complete server
-        # CHECK if len > x
+        logging.debug("received: {}".format(data.decode()))
         tx_type = data[:2]
         chunk = data[2:]
-        print(tx_type)
-        print(chunk)
+
+        logging.debug("tx_type: {}".format(tx_type))
+        logging.debug("chunk: {}".format(chunk))
         if tx_type == RRQ:
             server = RRQServer
         elif tx_type == WRQ:
             server = WRQServer
         else:
-            print("error")
+            logging.error("Received malformed packet")
             raise Exception("WUT")
         connect = loop.create_datagram_endpoint(
             lambda: server(chunk, addr),
             remote_addr=addr)
 
         self.loop.create_task(connect)
-        # print("recved!")
-        # print(len(data))
-        # print(data.decode())
-        # self.transport.sendto("nope\n".encode(encoding="ascii"), addr)
-        # self.transport.close()
 
     def connection_lost(self, exc):
-        print("conn lost")
+        logging.info('TFTP server - connection lost')
 
-loop = asyncio.get_event_loop()
+if __name__ == '__main__':
+    # argparse
+    port = 8069
+    i_addr = '127.0.0.1'
 
-listen = loop.create_datagram_endpoint(
-    lambda: TftpServer(loop),
-    local_addr=('127.0.0.1', 8069,))
+    logging.info('Starting TFTP server on {i_addr}:{port}'.format(
+        i_addr=i_addr, port=port))
+    loop = asyncio.get_event_loop()
+    listen = loop.create_datagram_endpoint(
+        lambda: TftpServer(loop),
+        local_addr=(i_addr, port,))
 
-transport, protocol = loop.run_until_complete(listen)
+    transport, protocol = loop.run_until_complete(listen)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    print("Quiting")
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        log.info('Received signal, shutting down')
 
-transport.close()
-loop.close()
+    transport.close()
+    loop.close()
