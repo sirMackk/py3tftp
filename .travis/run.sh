03a56bb227398cb57019b2664ab98d6dad128fc2
sirMackk <c.tairix@gmail.com>
2016-10-04 11:42:58 +0200

    Adds travis configuration files.

---
+++ .travis/run.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+###
+# Run unit tests and output coverage report
+###
+coverage run -m unittest discover tests/unit -p *test.py
+coverage report
+
+###
+# Run acceptance tests:
+# - Run py3tftp server on local host on default port.
+# - Run acceptance tests.
+# - Kill server and tests after 15 seconds.
+###
+timeout=15
+
+timeout -s SIGINT "${timeout}" python -m py3tftp --host 127.0.0.1 &> /dev/null &
+sleep 1
+timeout -s SIGINT "${timeout}" python -m unittest discover -s tests/acceptance -p *_test.py
