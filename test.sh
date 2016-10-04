2f9653fa8bfbe170e8afa41e74173f5c0121a1f3
sirMackk <c.tairix@gmail.com>
2016-10-04 11:45:45 +0200

    Removes py34 from travis tests file; Removes test.sh in favor of .travis/run.sh.

--- test.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-timeout=15
-
-timeout -s SIGINT "${timeout}" python -m py3tftp &> /dev/null &
-sleep 1
-timeout -s SIGINT "${timeout}" python -m unittest discover -s tests/acceptance -p *_test.py
